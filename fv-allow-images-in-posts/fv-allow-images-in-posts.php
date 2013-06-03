e751007fb2fc92a729e79b7e25752eb36078f1ad
Vlado Varga <vlado.varga@foliovision.com>
2013-06-03 18:42:16 +0200

    First version of plugin.

---
+++ fv-allow-images-in-posts/fv-allow-images-in-posts.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: FV Allow Images In Posts
+ * Plugin Description: Allows forum users to post <code>&lt;img /&gt;</code> tags in their comments.
+ * Author: Foliovision
+ * Author URI: http://foliovision.com   
+ * Version: 1.0
+ * Plugin URI: http://foliovision.com 
+ */ 
+ 
+add_filter( 'bb_allowed_tags', 'fv_allow_images_filter' );
+
+function fv_allow_images_filter( $tags ) {
+
+  //if (bb_current_user_can('moderate')) {
+  $tags['img'] = array( 'src' => array(),
+                        'title' => array(),
+                        'alt' => array() 
+                      );
+  return $tags;
+}
+?>
