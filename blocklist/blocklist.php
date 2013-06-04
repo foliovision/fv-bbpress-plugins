584891b628422d83405098b0253f8c4c497578bd
Vlado Varga <vlado.varga@foliovision.com>
2013-06-04 11:05:23 +0200

    Modified version of FV - the blocklist-admin.php merged into blocklist.php file.

--- blocklist/blocklist.php
+++ blocklist/blocklist.php
@@ -3,13 +3,103 @@
 Plugin Name: Blocklist
 Plugin URI:  http://bbpress.org/plugins/topic/blocklist
 Description:  blocks posts based on a list of words or IP addresses (like WordPress) by immediately marking them as spam
-Version: 0.0.4
+Version: 100.0.4 modified by FV
 Author: _ck_
 Author URI: http://bbshowcase.org
 */
 
-add_filter( 'bb_insert_post', 'blocklist_check', 8);
 
+if ( bb_is_admin() ) {
+	add_action('bb_admin_menu_generator', 'bb_blocklist_configuration_page_add');
+	add_action('bb_admin-header.php', 'bb_blocklist_configuration_page_process');
+}
+
+
+function bb_blocklist_configuration_page_add() {
+	bb_admin_add_submenu(__('Blocklist','blocklist'), 'keymaster', 'bb_blocklist_configuration_page', 'options-general.php');
+}
+
+
+function bb_blocklist_configuration_page() {
+	$blocklist = bb_get_option('blocklist');
+	?>	
+		<h2>Blocklist</h2>
+		<form class="settings" method="post" name="blocklist_form" id="blocklist_form">
+		<input type=hidden name="blocklist" value="1">
+			<table class="widefat">
+				<tbody>
+				<tr>
+					<td valign="top">
+					<div style="width:380px">
+					<label for="data"><b>Blocklist</b></label>
+					<br /><br />
+					enter only one item per line
+					<br /><br /> 
+					username, email, IP address, subject, and post text are&nbsp;tested and&nbsp;if matched, post is sent to spam
+					<br /><br /> 
+					you can use partial words and partial ip addresses, which&nbsp;match&nbsp;to the left (ie. "starting with")
+					<br /><br /> 
+					lines that begin with <b>#</b><br />or <i>less than 4 characters</i> are <b>ignored</b>
+					</div>
+					</td>
+					<td align="left">
+					<div style="max-width:500px;">
+					<textarea rows="15" cols="40" style="padding:3px;font-size:1.5em;" name="data"><?php echo $blocklist['data']; ?></textarea>											
+					</div>					
+					</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td valign="top">
+					<label for="email"><b>Email notification</b></label>
+					<br /><br />
+					these emails will be notified when a post is blocked<br />					
+					only one email per line, leave blank for none					
+					</td>
+					<td align="left">
+					<div style="max-width:500px;">
+					<textarea rows="3" cols="40" style="padding:3px;font-size:1.5em;" name="email"><?php echo $blocklist['email']; ?></textarea>
+					</div>					
+					</td>
+					<td></td>
+				</tr>
+				<tr style="border:0">
+					<td></td>
+					<td align="left">
+					<div style="max-width:500px;">					
+						<p class="submit" style="border:0"><input class="submit" type="submit" name="submit" value="Save Blocklist Settings"></p>					
+					</div>					
+					</td>
+					<td></td>
+				
+				</tr>
+				</tbody>
+			</table>		
+		
+		</form>
+		<?php
+}
+
+
+function bb_blocklist_configuration_page_process() {
+	if (isset($_POST['submit']) && isset($_POST['blocklist'])) {
+	$options=array('data','email');
+	foreach ($options as $option) {
+		if (!empty($_POST[$option])) {
+			(array) $data=explode("\n",trim($_POST[$option]));
+			array_walk($data,create_function('&$arr','$arr=trim($arr);'));		
+			$blocklist[$option]=implode("\r\n",$data)."\r\n";
+		} else {$blocklist[$option]="";}
+	}
+	bb_update_option('blocklist',$blocklist);	
+   bb_admin_notice( __( '<strong>Settings saved.</strong>','xnpn') );	
+	}
+}
+
+
+
+add_filter( 'bb_insert_post', 'blocklist_check', 8);
+/*
 if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],"/bb-admin/")===false)) { // "stub" only load functions if in admin 
 	if (isset($_GET['plugin']) && ($_GET['plugin']=="blocklist_admin" || strpos($_GET['plugin'],"blocklist.php"))) {require_once("blocklist-admin.php");} 
 	elseif (defined('BACKPRESS_PATH') && strpos($_SERVER['REQUEST_URI'],'bb-admin/content.php')) {header('Location: '.str_replace('content.php','',$_SERVER['REQUEST_URI'])); exit;}
@@ -19,7 +109,7 @@ if ((defined('BB_IS_ADMIN') && BB_IS_ADMIN) || !(strpos($_SERVER['REQUEST_URI'],
 		bb_admin_add_submenu('Blocklist', 'administrate', 'blocklist_admin','content.php');
 	} 
 } 
-
+*/
 function blocklist_initialize() { 
 	global $blocklist; 
 	if (!isset($blocklist)) {$blocklist = bb_get_option('blocklist'); if (empty($blocklist)) {$blocklist['data']="";$blocklist['email']="";}}
