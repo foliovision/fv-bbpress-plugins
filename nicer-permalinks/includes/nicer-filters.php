dffb2420631f61fbd32fcb982c0a76e64a72b36a
Vlado Varga <vlado.varga@foliovision.com>
2013-06-04 11:10:15 +0200

    Original version of the plugin.

---
+++ nicer-permalinks/includes/nicer-filters.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package Nicer Permalinks
+ */
+
+
+// Add plugin filters
+add_filter( 'get_forum_link', 'get_forum_nicer_link', 10, 1 );
+add_filter( 'get_topic_link', 'get_topic_nicer_link', 10, 1 );
+add_filter( 'get_post_link',  'get_post_nicer_link', 10, 2 );
+
+
+/**
+ * Functions
+ */
+
+/**
+ * Return forum nicer link
+ *
+ * @param string $link Forum link
+ *
+ * @global $bb
+ *
+ * @return string
+ */
+function get_forum_nicer_link( $link ) {
+	global $bb;
+
+	// Remove redundant "forum" word from forum link and append '/'. Mandatory! Props: Mohta
+	return str_replace( $bb->uri . 'forum/', $bb->uri, $link ) . '/';
+}
+
+/**
+ * Return topic nicer link
+ *
+ * @param string $link Topic link
+ *
+ * @global $bb
+ * @global $topic
+ *
+ * @uses bb_get_topic_from_uri()
+ * @uses wp_get_referer()
+ * @uses add_query_arg()
+ * @uses bb_get_forum()
+ *
+ * @return string
+ */
+function get_topic_nicer_link( $link ) {
+	global $bb;
+
+	if ( function_exists( 'is_pm' ) // bbPM plugin is activated
+		&& false !== stripos( $_SERVER['REQUEST_URI'], $bb->path . 'pm/' ) // A bbPM page is requested
+	)
+		return $_SERVER['REQUEST_URI'] . '/'; // Append '/' to dodge .htaccess' rules
+
+	global $topic;
+
+	if ( empty( $topic ) || is_string( $topic ) ) { // bb-post.php names $topic a trimmed version of the post title, but here we are looking for the topic object
+		$topic = bb_get_topic_from_uri( $link );
+
+		if ( empty( $topic ) ) // Fix for bbPress 1.0.2 deleted topic redirection link
+			return add_query_arg( 'view', 'all', wp_get_referer() );
+	}
+
+	// Replace "topic" word with parent forum slug to emphasize hierarchy
+	return str_replace( $bb->uri . 'topic', $bb->uri . bb_get_forum( $topic->forum_id )->forum_slug, $link );
+}
+
+/**
+ * Return post nicer link
+ *
+ * @param string $link    Post link
+ * @param int    $post_id Post id
+ *
+ * @global $bb_post
+ *
+ * @uses bb_get_first_post()
+ * @uses get_post_link()
+ *
+ * @return string
+ */
+function get_post_nicer_link( $link, $post_id = 0 ) {
+	if ( empty( $post_id ) ) { // Fix for bbPress 1.0.2 Relevant "posts" links
+		global $bb_post; // $bb_post actually is a topic object
+
+		$link = get_post_link( bb_get_first_post( $bb_post )->post_id );
+	}
+
+	return $link;
+}
+?>
---
+++ nicer-permalinks/nicer-permalinks.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Plugin Name: Nicer Permalinks
+Plugin URI: http://bbpress.org/plugins/topic/nicer-permalinks/
+Description: Rewrites every bbPress URI removing the words "forum" and "topic" and emphasizes forum hierarchy. Based on <a href="http://www.technospot.net/blogs/">Ashish Mohta</a> and <a href="http://markroberthenderson.com/">Mark R. Henderson</a>'s <a href="http://blog.markroberthenderson.com/getting-rid-of-forums-and-topic-from-bbpress-permalinks-updated-plugin/">Remove Forum Topic</a> plugin.
+Version: 5.0.7
+Author: mr_pelle
+Author URI: http://scr.im/mrpelle
+*/
+
+/**
+ * @license CC-GNU-GPL http://creativecommons.org/licenses/GPL/2.0/
+ */
+
+
+/**
+ * Define constants
+ */
+define( 'NICER_PERMALINKS_ID',   'nicer-permalinks' );
+define( 'NICER_PERMALINKS_NAME', 'Nicer Permalinks' );
+
+
+// Create text domain for translations
+bb_load_plugin_textdomain( NICER_PERMALINKS_ID, dirname( __FILE__ ) . '/languages' );
+
+
+if ( bb_is_admin() ) // Load admin.php if on admin area
+	require_once( 'includes/admin.php' );
+
+if ( nicer_permalinks_enabled() ) // Load plugin core if plugin is enabled
+	require_once( 'includes/nicer-filters.php' );
+
+// Call uninstaller on plugin deactivation
+bb_register_plugin_deactivation_hook( __FILE__, 'nicer_permalinks_uninstall' );
+
+
+/**
+ * Functions
+ */
+
+/**
+ * Whether or not plugin is enabled
+ *
+ * @uses bb_get_option()
+ *
+ * @return boolean
+ */
+function nicer_permalinks_enabled() {
+	return (bool) bb_get_option( 'nicer_permalinks_enabled' );
+}
+?>
